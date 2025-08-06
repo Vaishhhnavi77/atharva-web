5f6403244bb88be339687eb20c60997591f76a90
Vaishnavihere01 <vaishnavilaxman.here@gmail.com>
2025-08-06 18:10:10 +0530

    demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 export default defineConfig({
   base: "/",
@@ -12,7 +11,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    componentTagger(),
   ],
   resolve: {
     alias: {
