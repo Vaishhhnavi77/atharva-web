377f13f0155d661240bb501a1bc7f2fc55ec96f1
Vaishhnavi <vaishnavinakhate.here@gmail.com>
2025-11-23 14:53:56 +0530

    Refactor Breadcrumb component for better type safety

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -9,6 +9,7 @@ const Breadcrumb = React.forwardRef<
   React.ComponentPropsWithoutRef<"nav"> & {
     separator?: React.ReactNode
   }
+  
 >(({ ...props }, ref) => <nav ref={ref} aria-label="breadcrumb" {...props} />)
 Breadcrumb.displayName = "Breadcrumb"
 
